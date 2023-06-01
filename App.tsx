331f2c47ca0e29c315d6cd4d35fc289fa63b2337
weaverswebsubham <subham.das@weavers-web.com>
2023-06-01 12:58:12 +0530

    splash screen done.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,14 @@
+import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
 
+// Importing the splash screen.
+import SplashScreen from 'react-native-splash-screen';
+
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <View>
       <Text>The app</Text>
